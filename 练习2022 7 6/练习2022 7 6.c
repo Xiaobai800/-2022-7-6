3eafff6b0fe956eb55a9c75781c806a81d20dbef
Xiaobai800 <2533760032@qq.com>
2022-07-06 10:36:36 +0800

    添加项目文件。

---
+++ 练习2022 7 6/练习2022 7 6.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int ret = 1;
+	int sum = 0;
+	for (n = 1; n <= 3; n++)
+	{
+		
+		ret = ret * n;
+		sum = sum + ret;
+
+	}
+	printf("sum=%d\n", sum);
+	return 0;
+}
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//
+//	}
+//	printf("sum=%d\n", sum); 
+//	return 0;
+//}
+
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret=ret*i;
+//	}
+//	printf("ret=%d\n",ret);
+//	return 0;
+//}
